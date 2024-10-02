829733d6dcf0a5e98e81fdfadefe9e8aa37bef29
rk3gaming <rkfrmda3@gmail.com>
2024-10-02 13:35:31 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.EnableAFK = true -- Leave on if you want this script to work.
+
+Config.afkTimeMinutes = 1 -- Minutes
+Config.kickTime = 60 -- Seconds
+Config.captchaLength = 6 -- Dont touch!
