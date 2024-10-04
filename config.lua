711359679269417639c370726d972909462a55e2
rk3gaming <rkfrmda3@gmail.com>
2024-10-04 00:20:31 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.EnableAFK = true -- Leave on if you want this script to work.
+
+Config.afkTimeMinutes = 10 -- Amount of minutes players have to be AFK for before receciving the CAPTCHA.
+Config.kickTime = 60 -- Seconds they have to answer the CAPTCHA before being kicked.
+Config.captchaLength = 6 -- Dont touch!
