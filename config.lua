b1c2fe1fe08c8b08b8b85c3142edf064899a3cc1
rk3gaming <rkfrmda3@gmail.com>
2024-10-04 00:19:38 -0500

    Delete config.lua

--- config.lua
+++
@@ -1,9 +0,0 @@
---- @class Config
---- @field afkTimeMinutes number Minutes of inactivity before CAPTCHA appears
---- @field kickTime number Seconds to complete CAPTCHA before kick
---- @field captchaLength number Length of the CAPTCHA string
-Config = {
-    afkTimeMinutes = 10,
-    kickTime = 60,
-    captchaLength = 6
-}
